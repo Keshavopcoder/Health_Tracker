2464160a73fb7efa6f11280585bdea7e0cf4dc74
Keshavopcoder <shuklakeshav70@gmail.com>
2025-03-11 19:41:04 +0530

    Initial commit

---
+++ src/main/java/com/htps/controller/UserController.java
@@ -0,0 +1,41 @@
+//package com.htps.controller;
+//import com.htps.dto.UserDTO;
+//import com.htps.service.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/api/users")
+//public class UserController {
+//
+//    @Autowired
+//    private UserService userService;
+//
+//    @GetMapping("/username/{username}")
+//    public UserDTO getUserByUsername(@PathVariable String username) {
+//        return userService.getUserByUsername(username);
+//    }
+//}
+
+
+
+///
+package com.htps.controller;
+
+import com.htps.dto.UserDTO;
+import com.htps.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/email/{email}")
+    public UserDTO getUserByEmail(@PathVariable String email) {
+        return userService.getUserByEmail(email);
+    }
+}
